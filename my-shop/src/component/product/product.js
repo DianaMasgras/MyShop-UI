ca1fd4ab513e9a9330d1ef61377d465fad2ed58b
DianaMasgras <dianamadalina30@gmail.com>
2022-03-04 17:12:51 +0200

    Add product card

--- my-shop/src/component/product/product.js
+++ my-shop/src/component/product/product.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 import ProductRestService from "../../service/product-rest-service";
 
 class Product extends React.Component {
@@ -14,7 +15,22 @@ class Product extends React.Component {
 
   renderProduct(product) {
     console.log('render product ' + product)
-    return <div key={product.name}>prod: {product.name}</div>
+    return <Card key={product.name}>
+    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{product.name}</Card.Header>
+      <Card.Description>
+        {product.description}
+      </Card.Description>
+      <Card.Meta>{product.price} $</Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        10 Friends
+      </a>
+    </Card.Content>
+  </Card>
   }
 
   render() {
@@ -22,7 +38,24 @@ class Product extends React.Component {
     return (
       <div>
         {this.state.products.map((product) => this.renderProduct(product))}
+        <Card>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>Daniel</Card.Header>
+            <Card.Description>
+              Daniel is a comedian living in Nashville.
+            </Card.Description>
+            <Card.Meta>Joined in 2016</Card.Meta>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              10 Friends
+            </a>
+          </Card.Content>
+        </Card>
       </div>
+
     )
   }
 }
