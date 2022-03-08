e0227d89526dbb63685d2e2e8d74f6d4cf040a20
DianaMasgras <dianamadalina30@gmail.com>
2022-03-08 12:27:28 +0200

    create product-page component and set up the route

--- my-shop/src/App.js
+++ my-shop/src/App.js
@@ -2,24 +2,24 @@ import './App.css';
 import Product from './component/product/product';
 import TopMenu from './component/top-menu/top-menu';
 import Footer from './component/footer/footer';
+import HomePage from './component/home-page/home-page';
+import ProductPage from './component/product-page/product-page';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Routes
 } from "react-router-dom";
-import HomePage from './component/home-page/home-page';
-
 
 function App() {
   return (
     <Router>
-      <TopMenu/>
-      <Footer/>
+      <TopMenu />
+      <Footer />
 
       <Routes>
         <Route path="/products" element={<Product />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/product" element={<ProductPage />} />
       </Routes>
     </Router>
   );
---
+++ my-shop/src/component/product-page/product-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ProductPage extends React.Component {
+    render() {
+        return (
+            <h1> product details</h1>
+        )
+    }
+}
+export default ProductPage;
